11a56dd5037f1459fa3e53ae9167793cd19aa7ec
WOD <1203734720@qq.com>
2022-01-30 16:32:41 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-button @click="logout" type="info">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    logout () {
+      // 删除token
+      window.sessionStorage.removeItem('token')
+      //   跳转到登录页
+      this.$router.replace('/login')
+      this.$message.warning('用户已退出！')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
